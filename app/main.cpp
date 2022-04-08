980a534881ae0462cf54c9c3b498713d53517bd7
Maciej Kazmierczak <m.kazmierczak93@gmail.com>
2022-04-08 15:50:19 +0200

    Simple setup

---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+#include "Logger.h"
+
+int main()
+{
+    Log("Hello World!");
+
+    return 0;
+}
---
+++ logger/include/Logger.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+static void Log(const std::string& _msg) { std::cout << _msg << std::endl; }
